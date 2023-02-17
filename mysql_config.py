be98cfa6d9187eb596f985f4ece8957da0a2c445
cjeduardo <cjeduardo.prog@gmail.com>
2023-02-17 11:29:37 +0100

    update mysql

--- mysql_config.py
+++ mysql_config.py
@@ -3,7 +3,7 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'containers-us-west-164.railway.app'
-app.config['MYSQL_DATABASE_PORT'] = '6688'
+app.config['MYSQL_DATABASE_PORT'] = 6688
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'nLJcClD5TrTTz8JdH57p'
 app.config['MYSQL_DATABASE_DB'] = 'railway'
